5951a90d7beabd030a09b34f7f603e097fa1d1bd
lakb248 <lakb248@163.com>
2016-07-01 16:27:44 +0800

    [bug fix] fix safari not support Date.toLocalString

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,14 @@ Vue.use(VueRouter);
 
 Vue.filter('timeStampFilter', function (input) {
     if (input) {
-        return new Date(input).toLocaleString();
+        var date = new Date(input);
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var hour = date.getHours();
+        var minute = date.getMinutes();
+        return date.getFullYear() + '/' + (month < 10 ? ('0' + month) : month)
+            + '/' + (day < 10 ? ('0' + day) : day) + ' ' + (hour < 10 ? ('0' + hour) : hour)
+            + ':' + (minute < 10 ? ('0' + minute) : minute);
     } else {
         return '';
     }
